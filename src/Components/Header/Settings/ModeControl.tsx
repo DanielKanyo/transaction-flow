872099d30d33f92e455f9b8eb2b441212734da29
DEV-XKDA <brueckner.de\dev-xkda@RH-DEV-XKDA.brueckner.de>
2025-09-18 12:04:41 +0200

    Settle transaction when mode is changing to avoid confusion

--- src/Components/Header/Settings/ModeControl.tsx
+++ src/Components/Header/Settings/ModeControl.tsx
@@ -1,8 +1,11 @@
+import { useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 
 import { SegmentedControl, Text } from "@mantine/core";
 
+import { settleTransaction, addNewBlock } from "../../../Store/Features/Ledger/LedgerSlice";
+import { clearMempool } from "../../../Store/Features/Mempool/MempoolSlice";
 import { Modes, updateAdvancedMode } from "../../../Store/Features/Settings/SettingsSlice";
 import { useAppSelector } from "../../../Store/hook";
 import classes from "./SettingsModal.module.css";
@@ -10,7 +13,23 @@ import classes from "./SettingsModal.module.css";
 function ModeControl() {
     const dispatch = useDispatch();
     const { advancedMode } = useAppSelector((state) => state.settings);
+    const { transactions } = useAppSelector((state) => state.mempool);
     const { t } = useTranslation();
+
+    const handleOnChange = useCallback(
+        (value: Modes) => {
+            dispatch(updateAdvancedMode(value !== Modes.BASIC));
+
+            if (transactions.length) {
+                transactions.forEach((tx) => {
+                    dispatch(settleTransaction(tx));
+                });
+                dispatch(clearMempool());
+                dispatch(addNewBlock(Boolean(transactions.length)));
+            }
+        },
+        [transactions]
+    );
 
     return (
         <div>
@@ -25,7 +44,7 @@ function ModeControl() {
                 radius="md"
                 color="violet"
                 value={advancedMode ? Modes.ADVANCED : Modes.BASIC}
-                onChange={(v) => dispatch(updateAdvancedMode(v !== Modes.BASIC))}
+                onChange={(v) => handleOnChange(v as Modes)}
             />
         </div>
     );
